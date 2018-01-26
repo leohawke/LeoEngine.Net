c4bc5fb722d64f0e94fc143c88b0d5172fbea3f8
姚旭 <lia77593@163.com>
2018-01-26 10:01:27 +0800

    init

---
+++ leo.Engine/Asset/MeshAsset.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using leo.Platform.Render.Vertex;
+using leo.Platform.Render;
+namespace leo.Asset
+{
+    public class MeshAsset
+    {
+        public class SubMeshDescrption
+        {
+            byte MaterialIndex { get; set; }
+            [StructLayout(LayoutKind.Sequential)]
+            public struct LodDescription
+            {
+                uint VertexNum { get; set; }
+                uint VertexBase { get; set; }
+                uint IndexNum { get; set; }
+                uint IndexBase { get; set; }
+            }
+
+            List<LodDescription> _lodsDescription = new List<LodDescription>();
+            public IEnumerable<LodDescription> LodsDescription => _lodsDescription;
+
+            public void AddLodDesciption(LodDescription desc) => _lodsDescription.Add(desc);
+        }
+
+        List<Element> _vertexElements = new List<Element>();
+        public IEnumerable<Element> VertexElemnts => _vertexElements;
+
+        public EFormat IndexFormat { get; private set;}
+
+        byte[] _indexStream;
+        public ReadOnlySpan<byte> IndexStream => _indexStream;
+
+        List<SubMeshDescrption> _subMeshs = new List<SubMeshDescrption>();
+        public IEnumerable<SubMeshDescrption> SubMeshs => _subMeshs;
+
+        List<ReadOnlySpan<byte>> _vertexStreams = new List<ReadOnlySpan<byte>>();
+        public IEnumerable<ReadOnlySpan<byte>> VertexStreams => _vertexStreams;
+
+        public void AddVertexStream(Element element, ReadOnlySpan<Byte> stream)
+        {
+            _vertexElements.Add(element);
+            _vertexStreams.Add(stream);
+        }
+
+        public void SetIndexStream(EFormat format, ReadOnlySpan<Byte> stream)
+        {
+            _indexStream = stream.ToArray();
+            IndexFormat = format;
+        }
+
+        public void AddSubMeshDescrption(SubMeshDescrption desc)
+        {
+            _subMeshs.Add(desc);
+        }
+    }
+}
---
+++ leo.Engine/Render/InputLayout.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Runtime.InteropServices;
+
+namespace leo.Platform.Render.Vertex
+{
+    public enum Usage
+    {
+        Position,//位置
+
+        Normal,//法线
+        Tangent,
+        Binoraml,
+
+        Diffuse,//顶点色
+        Specular,//顶点高光
+        BlendWeight,
+        BlendIndex,
+
+        TextureCoord,
+    };
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Element
+    {
+        Usage Usage { get; set; }
+        byte UsageIndex { get; set; }
+        EFormat Format { get; set; }
+    }
+
+}
